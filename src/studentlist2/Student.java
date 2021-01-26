51aa43fc7ed6855e9584d096af84364a1b4eae9f
haruunmohamed2002 <haruunmohamed2002@gmail.com>
2021-01-25 23:14:07 -0500

    Added an address field

--- src/studentlist2/Student.java
+++ src/studentlist2/Student.java
@@ -7,10 +7,21 @@ package studentlist2;
 public class Student {
     
     private String name;
+    
+    private String address;
 
     public Student(String name) {
         this.name = name;
     }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+    
     
     public String getName() {
         return name;
